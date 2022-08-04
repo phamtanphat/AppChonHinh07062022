b9720b545400281ab04dd043a44cc72d33d058f8
Phat <phatdroid94@gmail.com>
2022-08-04 21:11:38 +0700

    get drawable

--- app/src/main/java/com/example/appchonhinh07062022/MainActivity.java
+++ app/src/main/java/com/example/appchonhinh07062022/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.appchonhinh07062022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgRandom, imgPick;
+    String[] arrDrawable;
+    int resourceRandom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgRandom = findViewById(R.id.imgRandom);
+        imgPick = findViewById(R.id.imgPick);
+
+        arrDrawable = getResources().getStringArray(R.array.arrays_drawable);
+        resourceRandom = getResources().getIdentifier(arrDrawable[1], "drawable", getPackageName());
+        imgRandom.setImageResource(resourceRandom);
+
     }
 }
