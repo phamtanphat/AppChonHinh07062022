32401fd2631d16b85cf85fc36c25b74bf1af6b75
Phat <phatdroid94@gmail.com>
2022-08-06 20:07:29 +0700

    handle menu item

--- app/src/main/java/com/example/appchonhinh07062022/MainActivity.java
+++ app/src/main/java/com/example/appchonhinh07062022/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.appchonhinh07062022;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import java.util.Random;
@@ -22,8 +25,28 @@ public class MainActivity extends AppCompatActivity {
         imgPick = findViewById(R.id.imgPick);
         arrDrawable = getResources().getStringArray(R.array.arrays_drawable);
 
+        randomImage();
+    }
+
+    private void randomImage() {
         int indexRandom = new Random().nextInt(arrDrawable.length);
         resourceRandom = getResources().getIdentifier(arrDrawable[indexRandom], "drawable", getPackageName());
         imgRandom.setImageResource(resourceRandom);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_refresh:
+                randomImage();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
